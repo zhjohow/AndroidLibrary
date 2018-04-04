bfd4fa698b749b12a58824a7babed74126ca57a6
zhjohow <zhjohow@163.com>
2018-04-04 10:47:13 +0800

    Update ToastUtils.java

--- library/src/main/java/com/zhjh/library/utils/ToastUtils.java
+++ library/src/main/java/com/zhjh/library/utils/ToastUtils.java
@@ -1,6 +1,5 @@
 package com.zhjh.library.utils;
 
-import android.content.Context;
 import android.view.Gravity;
 import android.widget.Toast;
 
@@ -9,27 +8,27 @@ import android.widget.Toast;
  */
 public class ToastUtils {
     public static Toast toast;
-    public static void showToast(Context context,String msg) {
+    public static void showToast(String msg) {
         if (toast == null) {
-            toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
+            toast = Toast.makeText(BaseApplication.context(), "", Toast.LENGTH_SHORT);
         }
         toast.setText(msg);
         toast.setGravity(Gravity.BOTTOM, 0, 200);
         toast.show();
     }
 
-    public static void showDataToast(Context context) {
+    public static void showDataToast() {
         if (toast == null) {
-            toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
+            toast = Toast.makeText(BaseApplication.context(), "", Toast.LENGTH_SHORT);
         }
         toast.setText("数据异常，请稍后重试！");
         toast.setGravity(Gravity.BOTTOM, 0, 200);
         toast.show();
     }
 
-    public static void showNetworkToast(Context context,String msg) {
+    public static void showNetworkToast(String msg) {
         if (toast == null) {
-            toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
+            toast = Toast.makeText(BaseApplication.context(), "", Toast.LENGTH_SHORT);
         }
         toast.setText("网络异常，请稍后重试！");
         toast.setGravity(Gravity.BOTTOM, 0, 200);
@@ -41,9 +40,9 @@ public class ToastUtils {
      * @param context
      * @param text
      */
-    public static void showToastCenter(Context context, String text) {
+    public static void showToastCenter(String text) {
         if (toast == null) {
-            toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
+            toast = Toast.makeText(BaseApplication.context(), "", Toast.LENGTH_SHORT);
         }
         toast.setText(text);
         toast.setGravity(Gravity.CENTER, 0, 0);
